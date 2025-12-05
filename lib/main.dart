ae5c748272ff093d75a025b238ced68550826493
cendyal-hub <sendyaja17@gmail.com>
2025-12-05 20:22:29 +0700

    mengubah fungsi main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:firebase_login/firebase_options.dart';
+import 'package:firebase_login/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -11,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Firebase Login",
-      // home: "",
+      home: MyLogin(),
     );
   }
 }
